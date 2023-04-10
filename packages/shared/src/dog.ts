62e523dabb7f8bf6932c0d4a923a2d4eaae3abab
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-04-10 23:34:06 +0200

    Added properties to Dog interface

--- packages/shared/src/dog.ts
+++ packages/shared/src/dog.ts
@@ -1,5 +1,12 @@
 export interface Dog {
   id: string;
   name: string;
+  age: number;
   description: string;
+  category: string[];
+  gender: string;
+  breed: string;
+  adoptionPrice: number;
+  neutered: string;
+  mainImage?: { url: string; alt: string };
 }
