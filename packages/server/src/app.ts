94b7ad92cea26ea808cd547dcce54e993257f9b3
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-04-10 15:38:38 +0200

    Changed the app.ts file

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,20 +1,36 @@
-import express, {
-    Application,
-    Express,
-    json,
-    Request,
-    Response,
-    } from "express";
-    import dotenv from "dotenv";
-    import cors from "cors";
-    dotenv.config();
-    const app: Application = express();
-    const PORT: number = parseInt(process.env.PORT || "8800"); // makes sure it's a number
-    app.use(cors()); // TODO configure cors , this way is not secure
-    app.use(json()); // Parse json
-    app.get("/", (req: Request, res: Response) => {
-    res.json({ Message: "Hello World!" });
-    });
-    app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
-    });
+import express, { Application, json, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { setupMongoDb } from "./models/common";
+const multer = require("multer");
+
+dotenv.config();
+
+const app: Application = express();
+app.use(cors());
+app.use(json());
+
+const port: number = parseInt(process.env.SERVER_PORT || "3002");
+
+// image will upload to uploads folder locally, not to db yet
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (
+    req: any,
+    file: { originalname: any },
+    cb: (arg0: null, arg1: any) => void
+  ) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.use("/uploads", express.static("./uploads"));
+
+const MONGO_URL: string =
+  process.env.MONGO_URL || "mongodb://localhost:27017/happytails";
+
+app.listen(port, async function () {
+  await setupMongoDb(MONGO_URL);
+  console.log(`App is listening on port ${port} !`);
+});
