0dabe49b93af88b4cf1678faf09445c343bb80c2
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-04-18 10:47:18 +0200

    Renamed to dogRoute

--- packages/server/src/routes/dogRoute.ts
+++ packages/server/src/routes/dogRoute.ts
@@ -8,7 +8,7 @@ dogRouter.get("/", async (req: Request, res: Response) => {
     try {
         res.status(200).send(await loadDogs());
     } catch (error) {
-
+        res.status(500).send("Something went went wrong getting products");
     }
 });
 
