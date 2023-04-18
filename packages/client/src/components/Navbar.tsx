c6d6dd68a50353d287b5e1b70ffb65f2b635b819
Anna Rylander <annaelida.rylander@gmail.com>
2023-04-18 08:28:54 +0200

    hamburger menu on smaller screens

--- packages/client/src/components/Navbar.tsx
+++ packages/client/src/components/Navbar.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Flex,
   Spacer,
   Heading,
   Button,
-  ButtonGroup,
+  IconButton,
+  VStack,
+  useDisclosure,
 } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
 export default function Navbar() {
+  const { isOpen, onToggle } = useDisclosure();
+
   return (
     <Box>
       <Flex minWidth="max-content" alignItems="center" gap="2" p="2">
@@ -18,17 +23,38 @@ export default function Navbar() {
           </Heading>
         </Box>
         <Spacer />
-        <Flex alignItems="flex-end" gap="2">
+        <Flex
+          alignItems="flex-end"
+          gap="2"
+          display={{ base: "none", md: "flex" }}
+        >
           <Button bgColor="white">Hundarna</Button>
           <Button bgColor="white">Om oss</Button>
           <Button bgColor="white">Stöd</Button>
         </Flex>
-        {/* <Spacer />
-        <ButtonGroup gap="2" size="sm">
-          <Button colorScheme="teal">Sign Up</Button>
-          <Button colorScheme="teal">Log in</Button>
-        </ButtonGroup> */}
+        <IconButton
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          variant="outline"
+          aria-label="Open navigation"
+          onClick={onToggle}
+          display={{ base: "block", md: "none" }}
+        />
       </Flex>
+      {isOpen && (
+        <Box py="4" borderBottomWidth="1px">
+          <VStack alignItems="flex-start" spacing="4">
+            <Button bgColor="white" w="full">
+              Hundarna
+            </Button>
+            <Button bgColor="white" w="full">
+              Om oss
+            </Button>
+            <Button bgColor="white" w="full">
+              Stöd
+            </Button>
+          </VStack>
+        </Box>
+      )}
     </Box>
   );
 }
