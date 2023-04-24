eddf2d72378bfb94ba466bcf50022695746399d5
Anna Rylander <annaelida.rylander@gmail.com>
2023-04-24 10:14:26 +0200

    route and controller for getting a dog by id

--- packages/server/src/controllers/dogController.ts
+++ packages/server/src/controllers/dogController.ts
@@ -1,8 +1,16 @@
 import { Dog } from "@happy-tails/shared";
-import { loadAllDogs } from "../models/dog";
+import { loadAllDogs, loadSingleDog } from "../models/dog";
 
 const loadDogs = async (): Promise<Dog[]> => {
-    return await loadAllDogs();
-  };
+  return await loadAllDogs();
+};
 
-export { loadDogs };
+const loadDogById = async (id: string): Promise<Dog> => {
+  const dog = await loadSingleDog(id);
+  if (!dog) {
+    throw new Error("Dog not found");
+  }
+  return dog;
+};
+
+export { loadDogs, loadDogById };
--- packages/server/src/routes/dogRoute.ts
+++ packages/server/src/routes/dogRoute.ts
@@ -1,15 +1,24 @@
 import { Dog } from "@happy-tails/shared";
 import express, { Request, Response } from "express";
-import { loadDogs } from "../controllers/dogController";
+import { loadDogs, loadDogById } from "../controllers/dogController";
 
 const dogRouter = express.Router();
 
 dogRouter.get("/", async (req: Request, res: Response) => {
-    try {
-        res.status(200).send(await loadDogs());
-    } catch (error) {
-        res.status(500).send("Something went went wrong getting products");
-    }
+  try {
+    res.status(200).send(await loadDogs());
+  } catch (error) {
+    res.status(500).send("Something went went wrong getting products");
+  }
+});
+
+//get dog by id
+dogRouter.get("/:id", async (req: Request, res: Response) => {
+  try {
+    res.status(200).send(await loadDogById(req.params.id));
+  } catch (error) {
+    res.status(500).send("Something went went wrong");
+  }
 });
 
 export default dogRouter;
