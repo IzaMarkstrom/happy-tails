ac835426dcd4bd325840e90399799621a5915820
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-05-02 22:22:41 +0200

    Refactor isMobile to less code

--- packages/client/src/components/DonationDetails.tsx
+++ packages/client/src/components/DonationDetails.tsx
@@ -1,4 +1,4 @@
-import { Box, Icon, Text, Input, Grid, GridItem, useMediaQuery } from '@chakra-ui/react'
+import { Box, Icon, Text, Input, Grid, GridItem, useMediaQuery, Button } from '@chakra-ui/react'
 import { FaCcMastercard, FaCcPaypal, FaCcVisa } from "react-icons/fa";
 import React from 'react'
 
@@ -6,11 +6,8 @@ export default function DonationDetails(props: { value: number }) {
     const [isMobile] = useMediaQuery("(max-width: 768px)");
 
   return (
-    <>                                        
-        {isMobile ?  
-        <Text mb={2}>Donera {props.value} SEK till Happy Tails</Text>
-        :
-         <Text mb={7}>Donera {props.value} SEK till Happy Tails</Text>}
+    <>          
+        <Text mb={isMobile ? 2 : 7}>Donera {props.value} SEK till Happy Tails</Text>
         <Box display="flex" flexDir="column" justifyContent="flex-start" alignItems="start">
             <Grid w="100%"   templateRows='repeat(3, 1fr)' templateColumns='repeat(4, 1fr)' gap={2}>
                 <GridItem colSpan={4}>
@@ -30,6 +27,9 @@ export default function DonationDetails(props: { value: number }) {
                 Vi accepterar: <Icon as={FaCcPaypal} /> <Icon as={FaCcVisa} />{" "}
                 <Icon as={FaCcMastercard} />
             </Text>
+            <Button justifySelf="center" alignSelf="center" size={isMobile ? "sm" : "lg"} variant="solid"
+              bg="brand.primary"
+              color="white">Donate</Button>
         </Box>
     </>
   )
