bd22ac525402cf2b85e1b81a799bb76fc2a3c8cf
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-05-03 17:18:29 +0200

    Added /create, /login, /getUser routes for the user

---
+++ packages/server/src/routes/userRoute.ts
@@ -0,0 +1,83 @@
+import express, { Request, Response } from "express";
+import { saveUser, getUserByEmail } from "../services/user-service";
+import { authUser, generateToken, JwtRequest } from "../services/auth";
+import { UserItem } from "@happy-tails/shared";
+const { UserModel } = require("../models/user");
+const bcrypt = require("bcrypt");
+
+const userRouter = express.Router();
+
+export default userRouter;
+
+userRouter.post(
+  "/create",
+  async (req: Request<UserItem>, res: Response<any>) => {
+    const { email } = req.body;
+
+    const userExists = await UserModel.findOne({ email });
+
+    if (userExists) {
+      res.status(409).send("An account with this email already exist.");
+    } else {
+      try {
+        const newUser = await saveUser(req.body);
+        if (newUser) {
+          const userInfo = await getUserByEmail(newUser.email);
+          const token = await generateToken(newUser.email);
+          res.status(200).send({ token, userInfo });
+        } else {
+          res.status(400).send("Something went wrong. Please try again.");
+        }
+      } catch (e) {
+        res.status(400).send(`Error: ${e}`);
+      }
+    }
+  }
+);
+
+userRouter.post(
+  "/login",
+  async (req: JwtRequest<UserItem>, res: Response<string>) => {
+    const credentials = req.body;
+
+    const userExists = await UserModel.findOne({
+      email: credentials.email,
+    }).select("+password");
+
+    if (userExists) {
+      const validPassword = await bcrypt.compare(
+        credentials.password,
+        userExists.password
+      );
+      if (validPassword) {
+        try {
+          const token = generateToken(userExists.email);
+
+          res.status(200).send(token);
+        } catch (e) {
+          res.status(400).send(`Error: ${e}`);
+        }
+      } else {
+        res.status(403).send("Wrong password");
+      }
+    } else {
+      res.status(403).send("No user found with this email");
+    }
+  }
+);
+
+userRouter.get(
+  "/getuser",
+  authUser,
+  async (req: JwtRequest<UserItem>, res: Response<any>) => {
+    const user = req.jwt;
+    try {
+      const userEmail = await getUserByEmail(user?.email);
+      if (userEmail) {
+        res.status(200).send(userEmail);
+      }
+    } catch (error) {
+      res.status(403).send(error);
+    }
+  }
+);
