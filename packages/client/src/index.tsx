d0b4d0b96a07daee50b153fca58cf528cc227af8
Anna Rylander <annaelida.rylander@gmail.com>
2023-05-03 17:26:57 +0200

    clean up

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
 import { BrowserRouter } from "react-router-dom";
@@ -10,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render( 
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider theme={theme}>
@@ -23,4 +22,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
